0501976667fe054fb5c59c12f1d8f4c10c330488
Álvaro <51362780+SkulleD@users.noreply.github.com>
2020-05-30 01:14:05 +0200

    Add files via upload

---
+++ Principal3.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class Principal3 {
+    public static void main(String[] args) {
+        System.out.println("Sigo vivo");
+        Bol8_Ejercicio3 frame = new Bol8_Ejercicio3();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(350, 350);
+        frame.setVisible(true);
+    }
+}
